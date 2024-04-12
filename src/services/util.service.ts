83efb4caabb789810c21e58a71f926973a0e8918
om-pageup <obhandari@pageupsoft.com>
2024-04-12 18:37:14 +0530

    fixed bugs

--- src/services/util.service.ts
+++ src/services/util.service.ts
@@ -1,12 +1,14 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { ChatBoxI } from '../app/model/chat.model';
 import { NumberString } from '../app/model/util.model';
-import { IGetAllUser } from '../app/response/user.response';
+import { IGetAllUser, UserI } from '../app/response/user.response';
+import { ComponentBase } from '../app/shared/class/ComponentBase.class';
+import { GetLoggedInUserDetailI } from '../app/response/responseG.response';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UtilService {
+export class UtilService extends ComponentBase {
 
   public sendMessageE: EventEmitter<boolean> = new EventEmitter<boolean>();
   public isListennotificationE: EventEmitter<number> = new EventEmitter<number>();
@@ -24,8 +26,12 @@ export class UtilService {
   // public unreadMsgCount: number = 0;
   public isAlreadyExists: boolean = false;
   public loggedInUserId: number = -1;
+  public loggedInUserName: string = "";
   public receiverId: number = -1;
   public currentOpenedChat: number = -1;
 
-  constructor() { }
+  public getLoggedInUserDetialsF() {
+    return this.getAPICallPromise<GetLoggedInUserDetailI<UserI>>('/userDetails', this.headerOption);
+  }
+
 }
